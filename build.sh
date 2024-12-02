f369bc943125852e8ac0594832ef0d20046362d7
Ondřej Tuček <ondrejtucek9@gmail.com>
2024-12-02 22:50:52 +0100

    fix

--- build.sh
+++ build.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
+# Ukončí skript na první chybu
+set -e
+
+# Získání aktuálního adresáře
 PARENT="$(pwd)"
+
+# Přesun do složky s React aplikací
 cd spa
+
+# Spuštění buildu aplikace
 yarn build
-cd dist 
+
+# Ověření, že build proběhl úspěšně
+if [ $? -ne 0 ]; then
+  echo "Build selhal."
+  exit 1
+fi
+
+# Přesun do složky dist
+cd dist
+
+# Vytvoření cílové složky, pokud neexistuje
+mkdir -p "$PARENT/public"
+
+# Kopírování souborů do cílové složky
 cp -r * "$PARENT/public"
+
+echo "Build a kopírování bylo úspěšné."
