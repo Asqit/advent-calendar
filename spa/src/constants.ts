65a341303e7e7dbf8df00a8e4d559b957320bff1
Ondřej Tuček <ondrejtucek9@gmail.com>
2024-12-03 10:08:46 +0100

    fix data

--- spa/src/constants.ts
+++ spa/src/constants.ts
@@ -1,5 +1,5 @@
 import { QueryClient } from "@tanstack/react-query";
 
 export const queryClient = new QueryClient();
-//export const BASE_URL = "https://asqit-calendar.deno.dev";
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://asqit-calendar.deno.dev";
+//export const BASE_URL = "http://localhost:8000";
